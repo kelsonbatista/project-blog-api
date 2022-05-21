30c265297d4d05921372d67fa006bd3e7a604ac7
kelsonbatista <kelsonbatista@gmail.com>
2022-05-21 15:59:56 -0300

    feat(server.js): added general router

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const router = require('./routes');
 const app = require('./api');
 
 // não remova a variável `API_PORT` ou o `listen`
@@ -8,5 +9,7 @@ const port = process.env.API_PORT || 3000;
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use(router);
 
 app.listen(port, () => console.log('ouvindo porta', port));
