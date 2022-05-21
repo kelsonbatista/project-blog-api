217345d67f87c8b7cb0f0b8b91c0069759e28140
kelsonbatista <kelsonbatista@gmail.com>
2022-05-21 19:07:33 -0300

    feat(userrouter): added deleteUser route

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -9,4 +9,6 @@ userRouter.post('/', userController.createUser);
 
 userRouter.put('/:id', userController.editUser);
 
+userRouter.delete('/:id', userController.deleteUser);
+
 module.exports = userRouter;
