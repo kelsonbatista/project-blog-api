0957f09125756cdc2e973e9bf6902ddfececa196
kelsonbatista <kelsonbatista@gmail.com>
2022-05-21 13:59:22 -0300

    feat: added user model

---
+++ src/database/models/users.js
@@ -0,0 +1,32 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    displayName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+  }, {
+    timestamp: false,
+    tableName: 'Users'
+  });
+
+  return User;
+};
