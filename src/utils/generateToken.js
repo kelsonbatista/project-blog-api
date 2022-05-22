6478e6e3cd23f9b5b0d269f1f28358b1fa4da34c
kelsonbatista <kelsonbatista@gmail.com>
2022-05-22 18:01:03 -0300

    feat(utils): added generateToken function

---
+++ src/utils/generateToken.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+const secret = process.env.JWT_SECRET;
+
+// JWT = HEADER + PAYLOAD(BODY OU DIRETO DO BANCO) + SECRET
+// CONFIG DO HEADER
+const headersConfig = {
+  expiresIn: '2m',
+  algorithm: 'HS256',
+};
+
+const generateToken = (payload) => {
+  const token = jwt.sign({ data: payload }, secret, headersConfig);
+  return token;
+};
+
+module.exports = generateToken;
