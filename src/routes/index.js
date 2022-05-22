35c8a298162f0d73083c418a14006ae94199c790
kelsonbatista <kelsonbatista@gmail.com>
2022-05-22 18:03:12 -0300

    feat(router): added login main  route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const userRouter = require('./userRouter');
+const loginRouter = require('./loginRouter');
 
 const router = express.Router();
 
 router.use('/user', userRouter);
+
+router.use('/login', loginRouter);
 
 module.exports = router;
