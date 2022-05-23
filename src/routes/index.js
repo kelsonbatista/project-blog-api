320f075c17ebac6bfeff4e6dd419173afbc6cfbc
kelsonbatista <kelsonbatista@gmail.com>
2022-05-23 00:00:17 -0300

    feat(router): added categories main route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const userRouter = require('./userRouter');
 const loginRouter = require('./loginRouter');
+const categoryRouter = require('./categoryRouter');
 
 const router = express.Router();
 
 router.use('/user', userRouter);
 
 router.use('/login', loginRouter);
+
+router.use('/categories', categoryRouter);
 
 module.exports = router;
