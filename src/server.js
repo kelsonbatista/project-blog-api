aaa473855d811cd15bc91a46a35183fe829bbb60
kelsonbatista <kelsonbatista@gmail.com>
2022-05-23 15:51:32 -0300

    refactor(api/server): router and errorMiddleware call moved from server to api

--- src/server.js
+++ src/server.js
@@ -1,6 +1,4 @@
 require('dotenv').config();
-const router = require('./routes');
-const errorMiddleware = require('./middlewares/errorMiddleware');
 const app = require('./api');
 
 // não remova a variável `API_PORT` ou o `listen`
@@ -10,8 +8,5 @@ const port = process.env.API_PORT || 3000;
 app.get('/', (_request, response) => {
   response.send();
 });
-
-app.use(router);
-app.use(errorMiddleware);
 
 app.listen(port, () => console.log('ouvindo porta', port));
