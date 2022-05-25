f3a5a292848ceeaea9645ad8ba3e7f06d63cc389
kelsonbatista <kelsonbatista@gmail.com>
2022-05-25 19:24:03 -0300

    refactor(usercontroller): express-rescue package applied

--- src/database/controllers/userController.js
+++ src/database/controllers/userController.js
@@ -1,67 +1,43 @@
+const rescue = require('express-rescue');
 const { StatusCodes } = require('http-status-codes');
 const userService = require('../services/userService');
 const { getToken } = require('../../utils/handleToken');
 
-const getUsers = async (_req, res, next) => {
-  try {
+const getUsers = rescue(async (_req, res, _next) => {
     const users = await userService.getUsers();
     const newUser = users.map((user) => {
       const { password: passDB, ...userInfo } = user.dataValues;
       return userInfo;
     });
     return res.status(StatusCodes.OK).json(newUser);
-  } catch (error) {
-    console.log(`Error: ${error}`);
-    next(error);
-  }
-};
+});
 
-const getUserById = async (req, res, next) => {
-  try {
+const getUserById = rescue(async (req, res, _next) => {
     const { id } = req.params;
     const user = await userService.getUserById(id);
     const { password: passDB, ...userInfo } = user.dataValues;
     return res.status(StatusCodes.OK).json(userInfo);
-  } catch (error) {
-    console.log(`Error: ${error}`);
-    next(error);
-  }
-};
+});
 
-const createUser = async (req, res, next) => {
-  try {
+const createUser = rescue(async (req, res, _next) => {
     const { displayName, email, password, image } = req.body;
     await userService.createUser(displayName, email, password, image);
     const token = await getToken({ displayName, email, image });
     return res.status(StatusCodes.CREATED).json({ token });
-  } catch (error) {
-    console.log(`Error: ${error}`);
-    next(error);
-  }
-};
+});
 
-const editUser = async (req, res, next) => {
-  try {
+const editUser = rescue(async (req, res, _next) => {
     const { id } = req.params;
     const userInfo = req.body;
     const user = await userService.editUser(id, userInfo);
     return res.status(StatusCodes.OK).json(user);
-  } catch (error) {
-    console.log(`Error: ${error}`);
-    next(error);
-  }
-};
+});
 
-const deleteUser = async (req, res, next) => {
-  try {
+const deleteUser = rescue(async (req, res, _next) => {
     const { id } = req.params.id === 'me' ? req.user.dataValues : req.params;
     await userService.deleteUser(id);
     return res.status(StatusCodes.NO_CONTENT).json({ message: 'User successfully deleted' });
-  } catch (error) {
-    console.log(`Error: ${error}`);
-    next(error);
-  }
-};
+});
 
 module.exports = {
   getUsers,
